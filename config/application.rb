5d4353327f76cbf6d2c6e8e3e2feda8fe2c653d5
Lukas Svoboda <lukas.svoboda@gmail.com>
2018-03-29 23:51:06 +0200

    refactor: put STI DeliveryGateways subclasses to subdirectory

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,9 @@ module CallMePager
     config.active_job.queue_adapter = :sidekiq
 
     # config.active_record.schema_format = :sql
-    config.eager_load_paths.push(
-      *%W[#{config.root}/app/models/integrations
+    config.eager_load_paths.push(*%W[
+                                 #{config.root}/app/models/integrations
+                                 #{config.root}/app/models/delivery_gateways
     ])
     config.autoload_paths = config.eager_load_paths.dup
 
